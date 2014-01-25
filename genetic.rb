18808a7c849817a76c40b6a6802d2222a1df1106
Daniel Sellergren <danielsellergren@gmail.com>
2014-01-25 10:48:11 -0500

    Adds crossover function

--- genetic.rb
+++ genetic.rb
@@ -54,4 +54,56 @@ until generation > num_generations
     # p total_value
   end
 
+  # Use Roulette wheel algorithm to proportionately create next generation
+  new_generation = []
+  num_knapsacks.times do
+    rnd = rand();
+    rnd_sum = 0.0
+    rnd_selected = 0;
+
+    knapsacks.each do |knapsack|
+      rel_value = knapsack.total_value / sum_value
+      rnd_sum += rel_value
+      if rnd_sum > rnd
+        new_generation << knapsack
+      else
+        rnd_selected += 1
+      end
+    end
+  end
+
+  # Randomly select two knapsacks
+  rnd_knap_1 = (0...num_knapsacks).to_a.sample
+  rnd_knap_2 = rnd_knap_1
+  until (rnd_knap_2 != rnd_knap_1)
+    rnd_knap_2 = (0...num_knapsacks).to_a.sample
+  end
+
+  p knapsacks[rnd_knap_1].chromosome
+  p knapsacks[rnd_knap_2].chromosome
+
+  # Perform crossover
+  split_point = (0...num_items).to_a.sample
+
+  # p split_point
+
+  front_1 = knapsacks[rnd_knap_1].chromosome[0, split_point];
+  front_2 = knapsacks[rnd_knap_2].chromosome[0, split_point];
+  back_1 = knapsacks[rnd_knap_1].chromosome[split_point, num_items-1];
+  back_2 = knapsacks[rnd_knap_2].chromosome[split_point, num_items-1];
+  new_chr_1 = front_1 + back_2
+  new_chr_2 = front_2 + back_1
+  new_1 = Knapsack.new(new_chr_1)
+  new_2 = Knapsack.new(new_chr_2)
+
+  knapsacks[rnd_knap_1] = new_1
+  knapsacks[rnd_knap_2] = new_2
+
+  # p knapsacks[rnd_knap_1].chromosome
+  # p knapsacks[rnd_knap_2].chromosome
+
+  # p new_generation[0]
+
+  generation += 1
+
 end
