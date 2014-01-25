b6c822d42a6a222729ce0c9c3610bc6dbbaf19a5
Daniel Sellergren <danielsellergren@gmail.com>
2014-01-25 10:13:04 -0500

    Adds total value and weight storage to knapsacks

--- knapsack.rb
+++ knapsack.rb
@@ -1,9 +1,11 @@
 class Knapsack
 
-  attr_accessor :chromosome
+  attr_accessor :chromosome, :total_weight, :total_value
 
   def initialize(chromosome)
     @chromosome = chromosome
+    total_weight = 0.0
+    total_value = 0.0
   end
 
 end
